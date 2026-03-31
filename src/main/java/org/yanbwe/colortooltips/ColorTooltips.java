f6e885db69b94b3944f02bcb78092a61135e93b0
Yanbwe <GGxiaoyang1@163.com>
2026-03-31 22:42:05 +0800

    创建

---
+++ src/main/java/org/yanbwe/colortooltips/ColorTooltips.java
@@ -0,0 +1,44 @@
+package org.yanbwe.colortooltips;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(ColorTooltips.MODID)
+public class ColorTooltips {
+
+    public static final String MODID = "colortooltips";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ColorTooltips() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("ColorTooltips commonSetup");
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            LOGGER.info("ColorTooltips clientSetup");
+        }
+    }
+}
---
+++ src/main/java/org/yanbwe/colortooltips/Config.java
@@ -0,0 +1,18 @@
+package org.yanbwe.colortooltips;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+
+@Mod.EventBusSubscriber(modid = ColorTooltips.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config {
+
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+    static final ForgeConfigSpec SPEC = BUILDER.build();
+
+    @SubscribeEvent
+    static void onLoad(final ModConfigEvent event) {
+    }
+}
